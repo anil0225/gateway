56641b0eab606b49c6a8b3ee7a148a5ce3c73252
Jeremy Foster <jeremy.foster@live.com>
2017-02-04 07:08:25 -0800

    add more to api (not yet tested)

--- modules/api.js
+++ modules/api.js
@@ -2,18 +2,6 @@
 
 let restify = require('restify');
 
-api.post('/api/message', (req, res, next) => {
-    this.broker.publish({
-        properties: {
-            'source': 'api'
-        },
-        content: new Uint8Array([
-            Math.random() * 50,
-            Math.random() * 50
-        ])
-    });
-});
-
 module.exports = {
     broker: null,
     configuration: null,
@@ -22,14 +10,22 @@ module.exports = {
         this.broker = broker;
         this.configuration = configuration;
 
+        let server = restify.createServer();
+        server.use(restify.bodyParser());
+        server.post('/api/messages', (req, res, next) => {
+            this.broker.publish({
+                properties: { 'source': 'api' },
+                content: req.body
+            });
+        });
+        server.listen(80);
+
         return true;
     },
 
-    receive: function (message) {
-        console.log(`printer.receive - ${message.content.join(', ')}`);
-    },
+    receive: function (message) {},
 
     destroy: function () {
-        console.log('printer.destroy');
+        console.log('api.destroy');
     }
 };
