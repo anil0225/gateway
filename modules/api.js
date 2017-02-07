adde4ca2cf36c15678250ce5039c0a052646aedd
Jeremy Foster <jeremy.foster@live.com>
2017-02-06 23:45:33 -0800

    add and configure api

--- modules/api.js
+++ modules/api.js
@@ -8,17 +8,25 @@ module.exports = {
 
     create: function (broker, configuration) {
         this.broker = broker;
-        this.configuration = configuration;
+
+        //TODO: hard code mac address for now
+        this.configuration = Object.assign({},
+            { macAddress: 'AA:BB:CC:DD:EE:FF' },
+            configuration
+        );
 
         let server = restify.createServer();
         server.use(restify.bodyParser());
         server.post('/api/messages', (req, res, next) => {
             this.broker.publish({
-                properties: { 'source': 'api' },
-                content: req.body
+                properties: {
+                    'source': 'api',
+                    'macAddress': this.configuration.macAddress
+                },
+                content: new Buffer(req.body, 'utf-8')
             });
         });
-        server.listen(80);
+        server.listen(3000);
 
         return true;
     },
