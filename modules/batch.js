949d233630de22dacf51d592210b740a864520b9
Jeremy Foster <jeremy.foster@live.com>
2017-02-20 10:07:38 -0800

    add filter and batch modules

---
+++ modules/batch.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let Rx = require('rxjs/Rx')
+let messages = new Rx.Observable();
+let subscription;
+
+module.exports = {
+    broker: null,
+    configuration: null,
+
+    create: function (broker, configuration) {
+        this.broker = broker;
+        this.configuration = configuration;
+        this.subscription = this.messages
+            //use methods like buffer or window to batch data
+            .subscribe(msg => broker.publish(msg));
+        return true;
+    },
+
+    receive: msg => messages.onNext(msg),
+    destroy: this.subscription.unsubscribe
+};
