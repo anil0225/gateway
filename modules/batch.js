b251c82eceb66cb226d6d0a0cfe8a4bad51e690a
Kevin Leung <leung.kevin23@gmail.com>
2017-03-12 22:38:43 +0000

    bufferTime progress

--- modules/batch.js
+++ modules/batch.js
@@ -1,26 +1,28 @@
-'use strict';
+'use strict'
 
 let Rx = require('rxjs/Rx')
-let messages = new Rx.Observable();
 
-module.exports = {
-    broker: null,
-    configuration: null,
-
-    create: function (broker, configuration) {
-        this.broker = broker;
-        this.configuration = configuration;
-
-        if (messages) {
-            messages.flatMap(function (msg) {
-                this.subscribe(msg => broker.publish(msg));
-            });
-        }
-        return true;
-    },
-
-    receive: msg => messages.onNext(msg),
-    destroy: function () {
-        subscription.unsubscribe;
+class BatchModule {
+    create(broker, configuration) {
+        this.broker = broker
+        this.configuration = configuration
+        this.messages = new Rx.Subject()
+        this.subscription = this.messages
+	    // buffer for 5 seconds, start next buffer in 1 second
+	    .bufferTime(5000, 5000)
+            .subscribe(msg => {     
+		this.broker.publish(msg)
+            })
+        return true
     }
-};
+
+    receive(msg) {
+        this.subscription = this.messages.next(msg)
+    }
+
+    destroy() {
+        this.subscription.unsubscribe()
+    }
+}
+
+module.exports = new BatchModule()
