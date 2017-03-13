45e5b5cfc9d9bc91a00d2a6ead43b21872828141
Kevin Leung <leung.kevin23@gmail.com>
2017-03-13 21:01:36 +0000

    publishable payload created from buffer

--- modules/batch.js
+++ modules/batch.js
@@ -7,21 +7,40 @@ class BatchModule {
         this.broker = broker
         this.configuration = configuration
         this.messages = new Rx.Subject()
-	    .bufferTime(5000)
         this.subscription = this.messages
+	    .bufferTime(5000)
             .subscribe(msgs => {
+		let buff_properties
+		let buff_content = []
+
+		// if buffer has at least one data set, set properties
+		// assuming all data in buffer is from same device
+		if (msgs.length > 0) {
+			buff_properties = msgs[0].properties
+		}
+
+		// createa a new array of the data
+		msgs.forEach(function(msg) {
+			buff_content.push(msg.content)
+		})
+
 	       	console.log('*** Buffered:', msgs)
- 		
-		// msgs is missing property: properties and content 
-		this.broker.publish(msgs)
+
+		let payload = { "properties":buff_properties, "content":buff_content }
+		console.log('>>> PAYLOAD <<<', payload)
+
+		// content must be UTF8 encoded..
+		this.broker.publish(payload)
             })
         return true
     }
 
-    receive(msg) {
-	msg = JSON.parse(new Buffer(msg.content).toString('utf-8')) 
-        this.subscription = this.messages.next(msg)
-	console.log('BATCH RECIEVED')
+    receive(msg) { 
+	msg.content = JSON.parse(new Buffer(msg.content).toString('utf-8'))
+
+	// passing in msg with string properties: 'properties' and 'content'
+	this.subscription = this.messages.next(msg)
+	console.log('BATCH RECEIVED')
     }
 
     destroy() {
